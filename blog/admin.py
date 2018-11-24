add4dcff0b06f857ad577836749494cde458b1e9
guajiropa <guajiropa@hotmail.com>
2018-11-24 16:33:57 -0500

    updated comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
+"""
+AUTHOR      :   Robert James Patterson
+DATE        :   11/24/18
+SYNOPSIS    :   Work thru files for 'Dajngo 2 by Example' by Packt Publishing
+"""
 from django.contrib import admin
 from .models import Post
 
